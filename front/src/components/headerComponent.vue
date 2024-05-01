0c4ff6ed51afc7320020c625a0464ff2707350c0
VladMexon <78736104+VladMexon@users.noreply.github.com>
2024-05-01 21:09:54 +0500

    fix2

--- front/src/components/headerComponent.vue
+++ front/src/components/headerComponent.vue
@@ -43,6 +43,7 @@ export default {
     async logout() {
       await this.$store.dispatch('user/logout');
       this.$store.commit('notifications/CLEAR_NOTIFICATIONS');
+      this.$store.commit('commentsStore/CLEAR');
       this.$router.push({ name: "loginPage" });
       this.openModal()
     },
--- front/src/store/commentsStore.js
+++ front/src/store/commentsStore.js
@@ -11,17 +11,17 @@ export default {
         getCommentsByNotifId: (state) => (notifId) => {
             return state.comments.filter(item => item.id_notification == notifId);
         },
-        getUpdateIds(state){
+        getUpdateIds(state) {
             return state.notificationsToUpdate;
         },
-        getLastId(state){
+        getLastId(state) {
             return state.last_id;
         },
         getCommentsCountById: (state) => (id) => {
             let count = state.commentsCount.find(item => item.id_notification === id);
-            if(count){
+            if (count) {
                 return count.comments_count;
-            }else{
+            } else {
                 return 0;
             }
         }
@@ -32,47 +32,53 @@ export default {
         },
         ADD_COMMENTS(state, payload) {
             payload.forEach(item => {
-                if(!state.comments.map(comment=>comment.id_comment).includes(item.id_comment))
+                if (!state.comments.map(comment => comment.id_comment).includes(item.id_comment))
                     state.comments.push(item);
             })
         },
-        ADD_UPDATE_ID(state, payload){
+        ADD_UPDATE_ID(state, payload) {
             state.notificationsToUpdate.push(payload);
         },
-        DELETE_UPDATE_ID(state, payload){
+        DELETE_UPDATE_ID(state, payload) {
             state.notificationsToUpdate = state.notificationsToUpdate.filter(id => id != payload);
         },
-        SET_LAST_ID(state, payload){
+        SET_LAST_ID(state, payload) {
             state.last_id = payload;
         },
-        UPDATE_COMMENTS_COUNT(state, payload){
+        UPDATE_COMMENTS_COUNT(state, payload) {
             payload.forEach(item => {
                 state.commentsCount = state.commentsCount.filter(item2 => item2.id_notification != item.id_notification);
 
                 state.commentsCount.push(item);
             });
-            
+
         },
+        CLEAR(state) {
+            state.comments = [];
+            state.notificationsToUpdate = [];
+            state.commentsCount = [];
+            state.last_id = 0;
+        }
     },
     actions: {
-        async sendComment({ commit }, payload){
+        async sendComment({ commit }, payload) {
             try {
                 const response = await api.notification.newComment(payload);
-                if(response.status === 200){
-                    commit('ADD_COMMENTS', [{id_notification: payload.id_notification, id_comment: response.data.id_comment, text: payload.text, id_author: payload.id_author, time: Date.now()}]);
+                if (response.status === 200) {
+                    commit('ADD_COMMENTS', [{ id_notification: payload.id_notification, id_comment: response.data.id_comment, text: payload.text, id_author: payload.id_author, time: Date.now() }]);
                     commit('SET_LAST_ID', response.data.id_comment);
                     return true;
-                }else{
+                } else {
                     return false;
                 }
             } catch (e) {
                 console.log(e);
             }
         },
-        async getNewComments({ commit, getters }){
+        async getNewComments({ commit, getters }) {
             try {
-                const response = await api.notification.getNewComments({last_id: getters.getLastId, commentIds: getters.getUpdateIds});
-                if(response.data.length > 0){
+                const response = await api.notification.getNewComments({ last_id: getters.getLastId, commentIds: getters.getUpdateIds });
+                if (response.data.length > 0) {
                     commit('SET_LAST_ID', response.data[response.data.length - 1].id_comment);
                     commit('ADD_COMMENTS', response.data);
                 }
@@ -80,7 +86,7 @@ export default {
                 console.log(e);
             }
         },
-        async getCommentsCount({ commit }, payload){
+        async getCommentsCount({ commit }, payload) {
             try {
                 const response = await api.notification.getCommentsCount(payload);
                 console.log(response.data);
@@ -89,7 +95,7 @@ export default {
                 console.log(e);
             }
         },
-        async getComments({ commit }, payload){
+        async getComments({ commit }, payload) {
             try {
                 const response = await api.notification.getComments(payload);
                 commit('ADD_COMMENTS', response.data);
